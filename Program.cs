e7c49c473d40f893f40881c97e5915c30602b64c
filipluka <filipsrbija11@gmail.com>
2024-02-10 20:58:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
